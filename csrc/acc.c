070210636a246592d04448688334ac03046dbb91
Jan Wawszczak <janekw23@gmail.com>
2022-01-31 13:43:11 +0100

    Remove acc stuff from main. Move assert to separate file

--- csrc/acc.c
+++ csrc/acc.c
@@ -1,6 +1,14 @@
 #include "acc.h"
+#include <i2c_configure.h>
 
-void acc_init();
+#define I2C_SPEED_HZ 100000
+#define PCLK1_MHZ 16
+#define LIS35DE_ADDR 0x1c  // i2c accelerometer address
+
+void acc_init() {
+	i2c_configure(PCLK1_MHZ * 1e6); // multiplication converts MHZ to HZ
+
+}
 
 void acc_write(uint8_t reg, uint8_t val);
 
