a4e8879bb225b917e1f16d82db3894fae90337a8
Jan Wawszczak <janekw23@gmail.com>
2022-02-11 01:12:18 +0100

    Close to being done. First version of improved mouse control script.

--- accmouse.py
+++ accmouse.py
@@ -1,18 +1,84 @@
 import sys
+import math
 
+import numpy as np
 import mouse
 
 
-if len(sys.argv) < 2:
-	print("Usage: accmouse.py <tty device file>")
 
-with open(sys.argv[1], 'r') as ttydevice:
-	for line in ttydevice:
-		if len(line) == 1: # Omit 'empty' lines
-			continue
+class AccMouse:
+	def __init__(self, buffer_len):
+		self.frame_buffer = np.zeros((buffer_len, 3))
+		self.counter = 0
+		self.stationary = True
+		self.stationary_acc = np.zeros(3)
+		self.velocity = np.zeros(2)
 
-		acc = int(line.strip())
-		acc = (acc + 127) % 256 - 128
-		
-		mouse.move(1080 * 0, 720 * 0 + acc, absolute=False)
-		
+	def update(self, acc):
+		self.counter += 1
+		acc = np.array([acc['x'], acc['y'], acc['z']])
+
+		self.frame_buffer = np.roll(self.frame_buffer, 1, axis=0)
+		self.frame_buffer[0, :] = acc
+
+		# wait until frame buffer has enough frames
+		if self.counter < len(self.frame_buffer):
+			return
+
+		# print(self.frame_buffer)
+
+		if self.stationary:
+			acc_max = self.frame_buffer[-4:].max(axis=0)
+			acc_min = self.frame_buffer[-4:].min(axis=0)
+
+			threshold = 2 / 128
+			if ((acc_max - acc_min) > threshold).any():
+				self.stationary = False
+
+		else: # mouse is moving
+			acc_max = self.frame_buffer.max(axis=0)
+			acc_min = self.frame_buffer.min(axis=0)
+
+			threshold = 2 / 128
+			if ((acc_max - acc_min) <= threshold).all():
+				self.stationary = True
+
+		if self.stationary:
+			print('STATIONARY')
+		else:
+			print('MOVING')
+
+
+		if self.stationary:
+			moving_avg_coef = 0.99
+			self.stationary_acc = moving_avg_coef * self.stationary_acc + (1 - moving_avg_coef) * self.frame_buffer[-1]
+
+			self.velocity[:] = 0
+		else:
+			self.velocity += (self.frame_buffer[-1, :2] - self.stationary_acc[:2]) * 1/50
+
+		mouse.move(self.velocity[1] * -7000, self.velocity[0] * -7000, absolute=False)
+
+
+
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print("Usage: accmouse.py <tty device file>")
+
+	mouse_controller = AccMouse(buffer_len = 5)
+
+	with open(sys.argv[1], 'r') as ttydevice:
+		for line in ttydevice:
+			line = line.strip().split()
+
+			acc = {'x': int(line[0]), 'y': int(line[1]), 'z': int(line[2])}
+
+			for axis in acc:
+				acc[axis] = ((acc[axis] + 127) % 256 - 128) / 128
+
+			print(acc)
+			mouse_controller.update(acc)			
+
+			mouse.move(1080 * 0, 720 * 0 + acc['x'], absolute=False)
+			
--- csrc/main.c
+++ csrc/main.c
@@ -4,58 +4,119 @@
 
 // ######################################## UTILS ########################################
 
-int uint_to_string(int num, char *buf) {
-	static char arr[16];
-	// num must be >= 0
+// int uint_to_string(uint32_t num, char *buf) {
+// 	static char arr[16];
+// 	// num must be >= 0
 
-	int index;
-	if (num == 0) {
-		arr[0] = 0;
-		index = 1;
-	} else {
-		for (index = 0; num != 0 && index < 10; index++) {
-			arr[index] = num % 10;
-			num /= 10;
+// 	int index;
+// 	if (num == 0) {
+// 		arr[0] = 0;
+// 		index = 1;
+// 	} else {
+// 		for (index = 0; num != 0 && index < 10; index++) {
+// 			arr[index] = num % 10;
+// 			num /= 10;
+// 		}
+// 	}
+
+// 	buf[index] = '\0';
+
+// 	for (int i = 0; i < index; i++) {
+// 		buf[i] = arr[index - i - 1] + 48; // convert digit to character representing that digit
+// 	}
+
+// 	return index; // effectively return string length
+// }
+
+// void send_uint(uint32_t num) {
+// 	static char buf[2048];
+// 	static int buf_index = 0;
+
+// 	char *msg = buf + buf_index;
+// 	buf_index += 32;
+// 	if (buf_index == 2048) buf_index = 0;
+
+// 	int length = uint_to_string(num, msg); // length is at most 10
+// 	send(msg, length); // omitting null byte
+// }
+
+#define BUF_LEN 2048
+
+void send_acc_reading(acc_reading_t reading) {
+	static char buf[BUF_LEN];
+	static uint32_t buf_index = 0;
+
+	const uint8_t msg_len = 12; // message has format: xxx<space>xxx<space>xxx<newline> which gives length 12
+
+	// Make sure message fits in the buffer.
+	if (buf_index + msg_len >= BUF_LEN)
+		buf_index = 0;
+
+	const char *msg = &buf[buf_index];
+
+	const int axes_count = 3;
+	for (uint8_t i = 0; i < axes_count; i++) {
+		uint8_t val;
+		switch (i) {
+			case 0: val = reading.x; break;
+			case 1: val = reading.y; break;
+			case 2: val = reading.z; break;
 		}
+
+		for (uint8_t p = 100; p > 0; p /= 10) {
+			buf[buf_index] = '0' + (val / p) % 10;
+			buf_index++;
+		}
+		buf[buf_index] = (i == (axes_count - 1) ? '\n' : ' ');
+		buf_index++;
 	}
 
-	buf[index] = '\0';
-
-	for (int i = 0; i < index; i++) {
-		buf[i] = arr[index - i - 1] + 48; // convert digit to character representing that digit
-	}
-
-	return index; // effectively return string length
+	send(msg, msg_len);
 }
 
-void send_uint(int num) {
-	static char buf[2048];
-	static int buf_index = 0;
 
-	char *msg = buf + buf_index;
-	buf_index += 32;
-	if (buf_index == 2048) buf_index = 0;
+// ######################################## COUNTER ########################################
 
-	int length = uint_to_string(num, msg); // length is at most 10
-	send(msg, length); // omitting null byte
+void start_counter() {
+	// Włączenie taktowania
+	RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+	// I Skonfigurowanie
+	TIM3->CR1 = 0; // tryb zliczania w górę
+	TIM3->PSC = 15999; // zmiana częstotliwości na 1KHz
+	TIM3->ARR = 9; // zliczanie do 320000 przy 16MHZ daje częstotliowść 50Hz TODO tu skończyłem, tylko czemu to nie działa?
+	TIM3->EGR = TIM_EGR_UG;
+	// I włączenie przerwań
+	TIM3->SR = ~TIM_SR_UIF; // wyzerowanie znaczników
+	TIM3->DIER = TIM_DIER_UIE; // włączenie przerwań
+	// I Uruchomienie
+	TIM3->CR1 |= TIM_CR1_CEN;
+
+	IRQsetPriority(TIM3_IRQn, LOW_IRQ_PRIO, LOW_IRQ_SUBPRIO);
+	NVIC_EnableIRQ(TIM3_IRQn);
+}
+
+// This function is called every time counter reaches its target value.
+void on_counter_tick() {
+	acc_read_xyz();
+}
+
+void TIM3_IRQHandler(void) {
+	uint32_t it_status = TIM3->SR & TIM3->DIER;
+	if (it_status & TIM_SR_UIF) { // zdarzenie uaktualnienia
+		TIM3->SR = ~TIM_SR_UIF;
+		on_counter_tick();
+	}
 }
 
 // ######################################## MAIN ########################################
 
 void on_acc_write_complete() {
-	acc_read_xyz();
+	// This will start reading accelerometer values with predefined frequency.
+	start_counter();
 }
 
 void on_acc_read_complete(acc_reading_t reading) {
-	send_uint(reading.x);
-	send(" ", 1);
-	send_uint(reading.y);
-	send(" ", 1);
-	send_uint(reading.z);
-	send(" ", 1);
-	send("\n\r", 2);
-
-	acc_read_xyz();
+	send_acc_reading(reading);
 }
 
 int main() {
